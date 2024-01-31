17ab81ac9db87310621295b4f7bd3714c2df2185
Xion Win <xionwin@hotmail.com>
2024-02-01 00:28:44 +0800

    Init

---
+++ app/src/main.rs
@@ -0,0 +1,11 @@
+pub use win32_rs::Window;
+
+fn main() {
+    println!("Hello, world!");
+
+    let display_handle =  egl_rs::get_display();
+    println!("display: {:#X?}", display_handle);
+
+    let window = Window::new(800, 480, "OpenGL ES 2.0");
+    window.show();
+}
