0dcbc9c8bddcb3da52c26b259d4a8b41b97c8f93
Xion <xionwin@hotmail.com>
2024-02-02 23:50:30 +0800

    Update

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,8 +3,8 @@ pub use win32_rs::Window;
 fn main() {
     println!("Hello, world!");
 
-    let display_handle =  egl_rs::get_display();
-    println!("display: {:#X?}", display_handle);
+    // let display_handle =  egl_rs::get_display();
+    // println!("display: {:#X?}", display_handle);
 
     let window = Window::new(800, 480, "OpenGL ES 2.0");
     window.show();
